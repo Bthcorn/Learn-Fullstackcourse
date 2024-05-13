2dc71754fcce16964a38964a6fca8d803d0dc069
Bthcorn <bowornthat.maize@gmail.com>
2024-05-13 22:34:20 +0700

    learn React day9

---
+++ day8/app/src/Hello.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+export default function Hello() {
+  // const [items, setItem] = useState([]);
+
+  // useEffect(() => {
+  //   console.log('startPage');
+  // }, [items]);
+
+  // const newItem = () => {
+  //   setItem([1, 2, 3, 4, 5]);
+  // }
+
+  return (
+    <div style={{backgroundColor: 'brown', color: 'white'}}>
+      Hello
+      {/* <button onClick={newItem}>New Item</button> */}
+    </div>
+  );
+}
