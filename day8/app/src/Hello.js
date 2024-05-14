9bfb18d37f86c347f18682ce1005c394add25274
Bthcorn <bowornthat.maize@gmail.com>
2024-05-14 23:19:51 +0700

    Test Get Method between Front and Back

--- day8/app/src/Hello.js
+++ day8/app/src/Hello.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+// const dayjs = require('dayjs');
+import axios from "axios";
 
 export default function Hello() {
   // const [items, setItem] = useState([]);
@@ -10,11 +12,20 @@ export default function Hello() {
   // const newItem = () => {
   //   setItem([1, 2, 3, 4, 5]);
   // }
+  // const [payDate,  setPayDate] = useState(new Date());
+  const getMethod = async () => {
+    try {
+      await axios.get("http://localhost:3001/book/orderBy");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <div style={{backgroundColor: 'brown', color: 'white'}}>
-      Hello
-      {/* <button onClick={newItem}>New Item</button> */}
+    <div>
+      <button className="btn btn-primary" onClick={getMethod}>
+        Call API GET METHOD
+      </button>
     </div>
   );
 }
