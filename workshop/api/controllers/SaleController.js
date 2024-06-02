4d0d328acad64189f49dadf591096470a5fe85c4
Bthcorn <bowornthat.maize@gmail.com>
2024-06-02 16:09:52 +0700

    Complete learning Day26

--- workshop/api/controllers/SaleController.js
+++ workshop/api/controllers/SaleController.js
@@ -3,49 +3,47 @@ const app = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-app.post('/save', async (req, res, next) => {
- try {
-  const rowBillSale = await prisma.billSale.create({
-    data: {
-      customerName: req.body.customerName,
-      customerAddress: req.body.customerAddress,
-      customerPhone: req.body.customerPhone,
-      payDate: new Date(req.body.payDate),
-      payTime: req.body.payTime,
-    },
-    }
-  );
-
-  for (let i = 0; i < req.body.carts.length; i++) {
-    const rowProduct = await prisma.product.findFirst({
-      where: {
-        id: req.body.carts[i].id
-      }
-    })
-
-    await prisma.billSaleDetail.create({
+app.post("/save", async (req, res, next) => {
+  try {
+    const rowBillSale = await prisma.billSale.create({
       data: {
-        billSaleId: rowBillSale.id,
-        productId: rowProduct.id,
-        qty: rowProduct.qty,
-        price: rowProduct.price,
-        cost: rowProduct.cost
-      }
-    })
+        customerName: req.body.customerName,
+        customerAddress: req.body.customerAddress,
+        customerPhone: req.body.customerPhone,
+        payDate: new Date(req.body.payDate),
+        payTime: req.body.payTime,
+      },
+    });
 
-  }
+    for (let i = 0; i < req.body.carts.length; i++) {
+      const rowProduct = await prisma.product.findFirst({
+        where: {
+          id: req.body.carts[i].id,
+        },
+      });
 
-  res.send({ message: 'success' });
- } catch (error) {
+      await prisma.billSaleDetail.create({
+        data: {
+          billSaleId: rowBillSale.id,
+          productId: rowProduct.id,
+          qty: rowProduct.qty,
+          price: rowProduct.price,
+          cost: rowProduct.cost,
+        },
+      });
+    }
+
+    res.send({ message: "success" });
+  } catch (error) {
     res.status(500).send({ message: error.message });
- }
+  }
 });
 
-app.get('/list', async (req, res, next) => {
+app.get("/list", async (req, res, next) => {
   try {
     const result = await prisma.billSale.findMany({
       orderBy: {
-        id: 'desc'
+        id: "desc",
       },
       // where: {
       //   status: 'wait'
@@ -57,18 +55,18 @@ app.get('/list', async (req, res, next) => {
   }
 });
 
-app.get('/billInfo/:billSaleId', async (req, res, next) => {
+app.get("/billInfo/:billSaleId", async (req, res, next) => {
   try {
     const result = await prisma.billSaleDetail.findMany({
       where: {
-        billSaleId: parseInt(req.params.billSaleId)
+        billSaleId: parseInt(req.params.billSaleId),
       },
       orderBy: {
-        id: 'desc'
+        id: "desc",
       },
       include: {
-        Product: true
-      }
+        Product: true,
+      },
     });
 
     res.send({ result: result });
@@ -77,52 +75,96 @@ app.get('/billInfo/:billSaleId', async (req, res, next) => {
   }
 });
 
-app.get('/updateStatustoPaid/:billSaleId', async (req, res, next) => {
+app.get("/updateStatustoPaid/:billSaleId", async (req, res, next) => {
   try {
     await prisma.billSale.update({
       where: {
-        id: parseInt(req.params.billSaleId)
+        id: parseInt(req.params.billSaleId),
       },
       data: {
-        status: 'paid'
-      }
+        status: "paid",
+      },
     });
-    res.send({ message: 'success' });
+    res.send({ message: "success" });
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
 });
 
-app.get('/updateStatustoSent/:billSaleId', async (req, res, next) => {
+app.get("/updateStatustoSent/:billSaleId", async (req, res, next) => {
   try {
     await prisma.billSale.update({
       where: {
-        id: parseInt(req.params.billSaleId)
+        id: parseInt(req.params.billSaleId),
       },
       data: {
-        status: 'sent'
-      }
+        status: "sent",
+      },
     });
-    res.send({ message: 'success' });
+    res.send({ message: "success" });
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
 });
 
-app.get('/updateStatustoCanceled/:billSaleId', async (req, res, next) => {
+app.get("/updateStatustoCanceled/:billSaleId", async (req, res, next) => {
   try {
     await prisma.billSale.update({
       where: {
-        id: parseInt(req.params.billSaleId)
+        id: parseInt(req.params.billSaleId),
       },
       data: {
-        status: 'canceled'
-      }
+        status: "canceled",
+      },
     });
-    res.send({ message: 'success' });
+    res.send({ message: "success" });
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
 });
 
-module.exports = app
+app.get("/dashboard", async (req, res, next) => {
+  try {
+    let arr = [];
+    let myDate = new Date();
+    let year = myDate.getFullYear();
+
+    for (let i = 1; i <= 12; i++) {
+      let daysInMonth = new Date(year, i, 0).getDate();
+
+      const billSaleinMonth = await prisma.billSale.findMany({
+        where: {
+          payDate: {
+            gte: new Date(year + "-" + i + "-01"),
+            lte: new Date(year + "-" + i + "-" + daysInMonth),
+          },
+        },
+      });
+
+      let sumPrice = 0;
+      
+      for (let j = 0; j < billSaleinMonth.length; j++) {
+        const sum = await prisma.billSaleDetail.aggregate({
+          _sum: {
+            price: true,
+          },
+          where: {
+            billSaleId: billSaleinMonth[j].id,
+          },
+        });
+        sumPrice = sum._sum.price ?? 0;
+      }
+
+      arr.push({
+        month: i,
+        total: sumPrice,
+      });
+    }
+
+    res.send({ result: arr });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+module.exports = app;
--- workshop/app/src/pages/backoffice/DashBoard.js
+++ workshop/app/src/pages/backoffice/DashBoard.js
@@ -2,17 +2,38 @@ import React from 'react'
 import BackOffice from '../../components/BackOffice'
 import { Bar } from 'react-chartjs-2'
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, plugins } from 'chart.js'
+import Swal from 'sweetalert2'
 
 ChartJS.register(BarElement, CategoryScale, LinearScale, Title, Tooltip, Legend, plugins)
 
 export default function DashBoard() {
+
+  const [DatafromApi, setDatafromApi] = React.useState([]);
+  // const [data, setData] = React.useState({});
+
+  React.useEffect(() => {
+    setDatafromApi([100, 200, 300, 400, 500]);
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: error.message,
+      });
+    }
+  }
 
   const data = {
     labels: ['January', 'February', 'March', 'April', 'May'],
     datasets: [
       {
         label: 'Monthly Sales',
-        data: [65, 59, 80, 81, 56],
+        data: DatafromApi,
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: 'rgba(255, 99, 132, 1)',
         boderWidth: 1
